c530d57074c2e8778d7d6fbfcad6cf87b5d2a99f
Rj <rikrdo.ortega@gmail.com>
2019-05-08 18:21:08 -0600

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,10 +55,3 @@ def runner
   end
   end_game(total)
 end
-    
-# Welcome the user
-# Deal them their first two cards, i.e. their initial_round
-# Ask them if they want to hit or stay
-# If they want to hit, deal another card
-# If they want to stay, ask them again!
-# If their card total exceeds 21, the game ends.
