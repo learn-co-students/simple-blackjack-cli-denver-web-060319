a7c073ca1671f63e4b093d8465957b2aa0338c99
“Patrick <pctennislessons@gmail.com>
2019-05-09 15:55:26 -0600

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,29 +1,37 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  puts num = rand(1..11)
+  num
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(num)
+  card_total = num
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  input = gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(num)
+  card_total = num
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_total = 0
+  2.times do
+    card_total = card_total + deal_card()
+  end
+  puts "Your cards add up to #{card_total}"
+  card_total
 end
 
 def hit?
@@ -41,4 +49,3 @@ end
 def runner
   # code runner here
 end
-    
